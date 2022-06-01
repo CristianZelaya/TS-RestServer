fbf5bc0d05945ee3b72a7988f9b90af1ab7154d2
Cristian Zelaya <criszel94@gmail.com>
2022-06-01 16:06:19 -0600

    Conexion a base de datos

--- models/server.ts
+++ models/server.ts
@@ -1,5 +1,7 @@
 import express, { Application } from 'express';
 import userRouter from '../routes/users';
+import cors from 'cors';
+import db from '../db/connection';
 
 class Server{
     
@@ -14,20 +16,57 @@ class Server{
         this.app = express();
         this.port = process.env.PORT || '8000';
 
+        this.dbConnection();
+
+        // Metodos iniciales
+        this.middlewares();
+
         // Definir mis rutas
         this.routes();
 
     }
 
+    async dbConnection() {
+
+        try {
+
+            await db.authenticate();
+            console.log('Database Online');
+            
+        } catch (error: any) {
+
+            throw new Error( error );
+            
+        }
+
+    }
+
+    //  Son funciones que se ejecutan antes que las rutas
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura del body
+        this.app.use( express.json() );
+
+        // Carpeta publica
+        this.app.use( express.static('public') );
+
+    }
+
     routes() {
-        this.app.use( this.apiPaths.users, userRouter )
+
+        this.app.use( this.apiPaths.users, userRouter );
+
     }
 
     listen() {
 
         this.app.listen( this.port, () => {
             console.log(`Servidor corriendo en http://localhost/${this.port}`)
-        })
+        });
+
     }
 
 }
